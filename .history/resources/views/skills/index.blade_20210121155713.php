e8537c0427be4381450a8a33e69a3712cb5d7880
Nicolas Faessel <niko@debian-tower>
2021-01-22 09:11:18 +0100

    removed .history

--- .history/resources/views/skills/index.blade_20210121155713.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-
-@section('content')
-<p> Ici on va afficher la liste des compétences </p>
-@endsection
--- .history/resources/views/skills/show.blade_20210121164715.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-
-@section('content')
-<p>{{$skill->label}}</p>
-<a href="{{route('skills.index')}}">Voir tous les skills</a>
-
-@endsection
